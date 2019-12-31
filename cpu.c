2b500943d64f804a299c98ba96f57fbccd9a6130
Mike Aleksiuk <maleksiuk@gmail.com>
2019-12-30 21:31:22 -0800

    Making it to test number 8 at this point.

--- cpu.c
+++ cpu.c
@@ -50,6 +50,7 @@ int main(int argc, char **argv)
   unsigned char carryFlag = 0;
   unsigned char overflowFlag = 0;
   unsigned char decimalFlag = 0; 
+  unsigned char interruptDisable = 0;
   unsigned char xRegister = 0;   // 8-bit index register
   unsigned char yRegister = 0;   // 8-bit index register
   unsigned char stackRegister = 0;  // stack register
@@ -62,6 +63,7 @@ int main(int argc, char **argv)
   for (int pc = memoryAddressToStartAt; pc < 50000;)
   {
     int i = pc;
+    int initialPc = pc;
 
     instr = buffer[i];
 
@@ -122,10 +124,10 @@ int main(int argc, char **argv)
       signed char relativeDisplacement = buffer[i+1];
       printf("%02x %02x\n", instr, buffer[i+1]);
       if (carryFlag == 0) {
-        printf("-> BCC %02x -> jump by %d\n", relativeDisplacement, relativeDisplacement);
+        printf("-> BCC %02x -> carry flag is zero, so jump by %d\n", relativeDisplacement, relativeDisplacement);
         pc += relativeDisplacement;
       } else {
-        printf("-> BCC %02x -> did not jump by %d\n", relativeDisplacement, relativeDisplacement);
+        printf("-> BCC %02x -> carry flag is not zero, so did not jump by %d\n", relativeDisplacement, relativeDisplacement);
       }
 
       pc += 2;
@@ -136,10 +138,10 @@ int main(int argc, char **argv)
       signed char relativeDisplacement = buffer[i+1];
       printf("%02x %02x\n", instr, buffer[i+1]);
       if (carryFlag == 1) {
-        printf("-> BCS %02x -> jump by %d\n", relativeDisplacement, relativeDisplacement);
+        printf("-> BCS %02x -> carry flag is set, so jump by %d\n", relativeDisplacement, relativeDisplacement);
         pc += relativeDisplacement;
       } else {
-        printf("-> BCS %02x -> did not jump by %d\n", relativeDisplacement, relativeDisplacement);
+        printf("-> BCS %02x -> carry flag is not set, so did not jump by %d\n", relativeDisplacement, relativeDisplacement);
       }
 
       pc += 2;
@@ -150,11 +152,6 @@ int main(int argc, char **argv)
       unsigned int memoryAddress = (buffer[i+2] << 8) | buffer[i+1];
       printf("%02x %02x %02x\n", instr, buffer[i+1], buffer[i+2]);
       printf("-> JMP to memory address: %x\n", memoryAddress);
-      if (memoryAddress == pc)
-      {
-        printf("ERROR IN TEST");
-        return(0);
-      }
       pc = memoryAddress;
     }
     // PHA; Push acc value to the stack; Len 1; Time 3
@@ -164,9 +161,61 @@ int main(int argc, char **argv)
       printf("-> PHA (push acc value %02x to stack at position %02x)\n", acc, stackRegister);
 
       unsigned int loc = 0x0100 + stackRegister;
-      memory[loc] = acc; 
+      memory[loc] = acc;
       stackRegister--;
 
+      pc++;
+    }
+    // PLA; Pull from stack and into acc; Len 1; Time 4
+    else if (instr == 0x68)
+    {
+      stackRegister++;
+      unsigned int loc = 0x0100 + stackRegister;
+      acc = memory[loc];
+      
+      printf("%02x\n", instr);
+      printf("-> PLA (pull from stack position %02x and into acc -> %02x)\n", stackRegister, acc);
+
+      zeroFlag = (acc == 0);
+      setNegativeFlag(acc, &negativeFlag);
+
+      pc++;
+    }
+    // PLP; Pull processor status; Len 1; Time 4
+    else if (instr == 0x28)
+    {
+      stackRegister++;
+      unsigned int loc = 0x0100 + stackRegister;
+      unsigned char value = memory[loc];
+
+      // NV1BDIZC
+      carryFlag = (value & 0x01) != 0;
+      zeroFlag = (value & 0x02) != 0;
+      interruptDisable = (value & 0x04) != 0;
+      decimalFlag = (value & 0x08) != 0;
+      overflowFlag = (value & 0x40) != 0;
+      negativeFlag = (value & 0x80) != 0;
+
+      printf("%02x\n", instr);
+      printf("-> PLP (pull from stack position %02x and into status flags)\n", stackRegister);
+
+      pc++;
+    }
+    // PHP; Push processor status; Len 1; Time 3
+    else if (instr == 0x08)
+    {
+      // NV1BDIZC
+      unsigned char value = (negativeFlag << 7) | (overflowFlag << 6) | (1 << 5) | (1 << 4)
+        | (decimalFlag << 3) | (interruptDisable << 2) | (zeroFlag << 1) | (carryFlag);
+
+      unsigned int loc = 0x0100 + stackRegister;
+      memory[loc] = value;
+
+      printf("%02x\n", instr);
+      printf("-> PHP (push status flags %02x to stack position %02x)\n", value, stackRegister);
+
+      // fd = 11111101
+      stackRegister--;
       pc++;
     }
     // CLD; Clear decimal flag; Len 1; Time 2
@@ -193,6 +242,16 @@ int main(int argc, char **argv)
       stackRegister = xRegister;
       printf("%02x\n", instr);
       printf("-> TXS -- (transfer %x to stack reg)\n", xRegister);
+      pc++;
+    } 
+    // TSX; Transfer Stack Pointer to X; Len 1; Time 2
+    else if (instr == 0xBA)
+    {
+      xRegister = stackRegister;
+      zeroFlag = (xRegister == 0);
+      setNegativeFlag(xRegister, &negativeFlag);
+      printf("%02x\n", instr);
+      printf("-> TSX -- (transfer %x to x reg)\n", stackRegister);
       pc++;
     } 
     // TAY; Transfer Acc to Y; Len 1; Time 2
@@ -225,6 +284,19 @@ int main(int argc, char **argv)
       printf("%02x %02x\n", instr, buffer[i+1]);
       printf("-> CMP #%02x (compare value %x to acc value %x)\n", operand, operand, acc);
       pc += 2;
+    }
+    // CMP; Compare acc with another value; absolute; Len 3; Time 4
+    else if (instr == 0xCD)
+    {
+      int operand = buffer[i+2] << 8 | buffer[i+1];
+      unsigned char value = memory[operand];
+      unsigned char result = acc - value;
+      zeroFlag = (result == 0);
+      carryFlag = (acc >= value);
+      setNegativeFlag(result, &negativeFlag);
+      printf("%02x %02x %02x\n", instr, buffer[i+1], buffer[i+2]);
+      printf("-> CMP %x (compare value at %x, %02x, to acc value %x)\n", operand, operand, value, acc);
+      pc += 3;
     }
     // CPY; Compare y with another value; immediate; Len 2; Time 2
     else if (instr == 0xC0)
@@ -290,6 +362,16 @@ int main(int argc, char **argv)
       printf("-> TAX -- (transfer acc to X, so X becomes %x)\n", xRegister);
       pc++;
     }
+    // TXA; Transfer X to acc; Len 1; Time 2 
+    else if (instr == 0x8A)
+    {
+      acc = xRegister;
+      zeroFlag = (acc == 0);
+      setNegativeFlag(acc, &negativeFlag);
+      printf("%02x\n", instr);
+      printf("-> TXA -- (transfer X to acc, so acc becomes %x)\n", acc);
+      pc++;
+    }
     // BPL; Branch on result plus; Len 2
     else if (instr == 0x10)
     {
@@ -326,6 +408,46 @@ int main(int argc, char **argv)
       {
         // no branch
         printf("-> BMI %02x -- (negative flag is not one, so do not jump)\n", buffer[i+1]);
+      }
+
+      pc += 2;
+    }
+    // BVC; Branch if overflow clear; Len 2
+    else if (instr == 0x50)
+    {
+      signed char relativeDisplacement = buffer[i+1];
+      printf("%02x %02x\n", instr, buffer[i+1]);
+
+      if (overflowFlag == 0) 
+      {
+        // branch
+        printf("-> BVC %02x -- (overflow flag is clear, so jump by %02x)\n", buffer[i+1], relativeDisplacement);
+        pc += relativeDisplacement;
+      } 
+      else 
+      {
+        // no branch
+        printf("-> BVC %02x -- (overflow flag is not clear, so do not jump)\n", buffer[i+1]);
+      }
+
+      pc += 2;
+    }
+    // BVS; Branch if overflow set; Len 2
+    else if (instr == 0x70)
+    {
+      signed char relativeDisplacement = buffer[i+1];
+      printf("%02x %02x\n", instr, buffer[i+1]);
+
+      if (overflowFlag == 1) 
+      {
+        // branch
+        printf("-> BVS %02x -- (overflow flag is set, so jump by %02x)\n", buffer[i+1], relativeDisplacement);
+        pc += relativeDisplacement;
+      } 
+      else 
+      {
+        // no branch
+        printf("-> BVS %02x -- (overflow flag is not set, so do not jump)\n", buffer[i+1]);
       }
 
       pc += 2;
@@ -397,16 +519,25 @@ int main(int argc, char **argv)
     }
     else {
       printf("unknown instruction: %x\n", instr);
+      printf("memory 0200: %02x\n", memory[0x0200]);
       return(0);
     }
 
     printState(xRegister, yRegister, acc, zeroFlag, negativeFlag, carryFlag, overflowFlag, pc, stackRegister);
+    printf("\nTop stack values: %02x %02x %02x %02x %02x\n", memory[0x01FF], memory[0x01FE], memory[0x01FD], memory[0x01FC], memory[0x01FB]);
     printf("\n\n");
+
+    if (initialPc == pc) {
+      printf("ERROR: did not move to a new instruction\n");
+      printf("memory 0200: %02x\n", memory[0x0200]);
+      return(0);
+    }
 
     instructionsExecuted++;
 
-    if (instructionsExecuted > 100000) {
+    if (instructionsExecuted > 900000) {
       printf("Stopping due to instruction limit.\n");
+      printf("memory 0200: %02x\n", memory[0x0200]);
       return(0);
     }
   }
