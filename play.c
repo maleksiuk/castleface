ad0d460fad5a096632c2073419d6a1720e4b17b2
Mike Aleksiuk <maleksiuk@gmail.com>
2020-01-03 17:00:48 -0800

    Make play.c start iterating through instructions at the correct place for Donkey Kong. Just learning about where to map the prgrom into system memory for mapper 0.

--- play.c
+++ play.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // the 6502 has 256 byte pages
 
@@ -9,7 +10,7 @@ int main(int argc, char **argv)
   printf("hi there\n");
 
   // TODO: read header into its own space, then prg rom into another space
-  unsigned char buffer[262160];
+  unsigned char header[16];
   FILE *file;
 
   unsigned char test;
@@ -17,148 +18,112 @@ int main(int argc, char **argv)
 
   file = fopen("donkey_kong.nes", "rb");
 
-  fread(buffer, sizeof(buffer), 1, file);
+  fread(header, sizeof(header), 1, file);
 
-  printf("Size of PRG ROM in 16kb units: %d\n", buffer[4]);
+  int sizeOfPrgRomInBytes = 16 * 1024 * header[4];
+  printf("Size of PRG ROM in 16kb units: %d (and in bytes: %d)\n", header[4], sizeOfPrgRomInBytes);
 
-  int sizeOfPrgRomInBytes = 16 * 1024 * buffer[4];
+  printf("Size of CHR ROM in 8 KB units (Value 0 means the board uses CHR RAM): %d\n", header[5]);
 
-  printf("Size of CHR ROM in 8 KB units (Value 0 means the board uses CHR RAM): %d\n", buffer[5]);
-
-  if ((buffer[7]&0x0C)==0x08) {
+  if ((header[7]&0x0C)==0x08) {
     printf("NES 2.0 format\n");
   } else {
     printf("Not NES 2.0 format\n");
   }
 
-  if ((buffer[6] & 0x01) == 0x01) {
+  if ((header[6] & 0x01) == 0x01) {
     printf("Vertical mirroring\n");
   } else {
     printf("Horizontal mirroring\n");
   }
 
-  if ((buffer[6] & 2) == 2) {
+  if ((header[6] & 2) == 2) {
     printf("Cartridge contains battery-backed PRG RAM ($6000-7FFF) or other persistent memory\n");
   } else {
     printf("Cartridge does not contain battery-packed PRG RAM\n");
   }
 
-  if ((buffer[6] & 4) == 4) {
+  if ((header[6] & 4) == 4) {
     printf("512-byte trainer at $7000-$71FF (stored before PRG data)\n");
   } else {
     printf("No 512-byte trainer at $7000-$71FF (stored before PRG data)\n");
   }
 
-  if ((buffer[6] & 8) == 8) {
+  if ((header[6] & 8) == 8) {
     printf("Ignore mirroring control or above mirroring bit; instead provide four-screen VRAM\n");
   } else {
     printf("Do not ignore mirroring control or above mirroring bit\n");
   }
 
-  int mapperNumber = buffer[7] << 8 | buffer[6];
+  int mapperNumber = header[7] << 8 | header[6];
   printf("mapper number: %d\n", mapperNumber);
 
+  // TODO: check return value of malloc
   unsigned char *memory;
-  memory = (unsigned char *) malloc(8192);
+  memory = (unsigned char *) malloc(0xFFFF);
+
+  unsigned char *prgRom;
+  prgRom = (unsigned char *) malloc(sizeOfPrgRomInBytes);
+  if (prgRom == 0) 
+  {
+    printf("Could not allocate memory for prgRom.");
+    // TODO: free the other memory I've allocated0
+    return(0);
+  }
+
+  printf("about to read into prgRom, num of bytes: %d\n", sizeOfPrgRomInBytes);
+  fread(prgRom, sizeOfPrgRomInBytes, 1, file);
+  fclose(file);
+
+  /*for (int i = 0; i < sizeOfPrgRomInBytes; i++)*/
+  /*{*/
+    /*printf("%02x ", prgRom[i]);*/
+  /*}*/
+
+  printf("prgRom: %x %x %x %x\n", prgRom[0], prgRom[1], prgRom[2], prgRom[3]);
+
+  // printf("1: %d, 2: %d, 3: %d, 4: %d\n", &(memory[0x8000]), memory + 0x8000, &(memory[0xC000]));
+  printf("memory[0x8000]: %x  memory: %p  &memory[0x8000]: %p\n", memory[0x8000], memory, &memory[0x8000]);
+
+  // put prgRom into memory at $8000-$BFFF and $C000-$FFFF   https://wiki.nesdev.com/w/index.php/NROM
+  memcpy(&memory[0x8000], prgRom, 0x4000);
+  memcpy(&memory[0xC000], prgRom, 0x4000);
+
+  printf("memory[0x8000]: %x %x %x %x\n", memory[0x8000], memory[0x8001], memory[0x8002], memory[0x8003]);
+  printf("memory[0xC000]: %x %x %x %x\n", memory[0xC000], memory[0xC001], memory[0xC002], memory[0xC003]);
 
   unsigned char instr = 0;
 
-  unsigned char acc = 0;
-  unsigned char z = 0;   // zero flag
-  unsigned char n = 0;   // negative flag
-
-  unsigned int *stack;
-  stack = (unsigned int *) malloc(100);
-  int stackLastIndex = 0;
-
   int instructionsExecuted = 0;
 
-  // 65532 and 65533 have the 16 bit reset address apparently
+  printf("last few bytes of prgRom: %x %x %x %x\n", prgRom[0x3FFD], prgRom[0x3FFE], prgRom[0x3FFF], prgRom[0x4000]);
 
-  printf("memory address to start at might be: %02x%02x\n", buffer[65533 + 16], buffer[65532 + 16]);
-  int memoryAddressToStartAt = (buffer[65533 + 16] << 8) | buffer[65532 + 16];
+  // 65532 and 65533 have the 16 bit reset address apparently (FFFC-FFFD)
+
+  printf("memory address to start at might be: %02x%02x\n", memory[0xFFFD], memory[0xFFFC]);
+  int memoryAddressToStartAt = (memory[0xFFFD] << 8) | memory[0xFFFC];
 
   printf("\n\nexecution:\n");
   /*sizeOfPrgRomInBytes = 50000; // TODO: tmp*/
-  for (int pc = memoryAddressToStartAt; pc < sizeOfPrgRomInBytes;)
+  for (int pc = memoryAddressToStartAt; pc < 0xFFFF;)
   {
-    int i = pc + 16;
+    int i = pc;
 
     // printf("byte %i: %x\n", i, buffer[i]);
-    printf("bytes %d to %d: %02x %02x %02x\n", pc, pc+2, buffer[i], buffer[i+1], buffer[i+2]);
+    printf("bytes %d to %d: %02x %02x %02x\n", pc, pc+2, memory[i], memory[i+1], memory[i+2]);
 
-    instr = buffer[i];
-
-    // LDA; Absolute; Len 3; Time 4
-    if (instr == 0xAD)
-    {
-      unsigned int memoryAddress = (buffer[i+2] << 8) | buffer[i+1];
-      acc = memory[memoryAddress];
-      if (acc == 0) {
-        z = 1;
-      }
-      // TODO: set n flag
-      printf("-> LDA %02x%02x -> acc = %d\n", buffer[i+2], buffer[i+1], acc);
-      pc += 3;
-    } 
-    // LDA; Immediate; Len 2; Time 2
-    else if (instr == 0xA9)
-    {
-      acc = buffer[i+1];
-      if (acc == 0) {
-        z = 1;
-      }
-      // TODO: set n flag
-      printf("-> LDA #%02x -> acc = %d\n", acc, acc);
-      pc += 2;
-    }
-    // BNE; Branch on not equal; Len 2
-    else if (instr == 0xD0)
-    {
-      signed char relativeDisplacement = buffer[i+1] + 2;  // adjust by 2 to make up for the two bytes this instruction takes
-      if (z == 0) {
-        printf("-> BNE %02x -> jump forward by %d\n", relativeDisplacement, relativeDisplacement);
-        pc += relativeDisplacement;
-      } else {
-        printf("-> BNE %02x -> did not jump forward by %d\n", relativeDisplacement, relativeDisplacement);
-        pc += 2;
-      }
-    }
-    // JSR; Jump to subroutine; Len 3; Time 6
-    else if (instr == 0x20)
-    {
-      stackLastIndex += 1;
-      stack[stackLastIndex] = pc + 2;  // save next operation pc minus one
-
-      unsigned int memoryAddress = (buffer[i+2] << 8) | buffer[i+1];
-
-      printf("-> JSR to memory address: %x\n", memoryAddress);
-      
-      pc = memoryAddress; 
-
-    } else {
-      pc++;
-    }
+    instr = memory[i];
 
     instructionsExecuted++;
-
     if (instructionsExecuted > 10) {
       return(0);
     }
+
+    pc++;
   }
 
-  // file size: 262160
-  // minus 16 byte header: 262144
-  //
-  // 131072 in PRG ROM
-  // 131072 in CHR ROM
-  // adds up to 262144!
-  // so the file is made up of these two chunks
-
   free(memory);
-  free(stack);
-
-  fclose(file);
+  free(prgRom);
 
   return(0);
 }
